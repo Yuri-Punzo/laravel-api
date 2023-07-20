048a719b91806cf9ad6e6c87a581a84ab7574f16
Yuri-Punzo <yuripunzo@hotmail.it>
2023-07-20 15:40:02 +0200

    added technology to db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,6 +23,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             ProjectSeeder::class,
+            TechnologySeeder::class,
             TypeSeeder::class,
         ]);
     }
