1b6c71ab9881dd8ba326dfe3e2da6b8a5bcb8ae4
Yuri-Punzo <yuripunzo@hotmail.it>
2023-08-03 16:54:08 +0200

    eliminated registration routes

--- routes/auth.php
+++ routes/auth.php
@@ -13,10 +13,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
     //commenta le dure rotte register per eliminare registrazione
-    Route::get('register', [RegisteredUserController::class, 'create'])
+    /* Route::get('register', [RegisteredUserController::class, 'create'])
         ->name('register');
 
-    Route::post('register', [RegisteredUserController::class, 'store']);
+    Route::post('register', [RegisteredUserController::class, 'store']); */
 
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
         ->name('login');
